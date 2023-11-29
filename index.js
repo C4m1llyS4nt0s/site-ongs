2d211f2acb54d35ad2d26ca3e3fe95fbca3976ca
camilly <camillychristines@gmail.com>
2023-11-29 18:39:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// let inputs = document.getElementByClassName("input-form")
+// for(let input of inputs) {
+//     input.addEventListener("blur", function(){
+//         if(input.value.trin() != ""){
+//             input.classList.add("has-val")
+//         }
+//         else{
+//             input.classList.remove("has-val")
+//         }
+    
+//     });
+// }
+    
