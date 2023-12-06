d3c4e7c2581b6d498920a4623157924d20fbfe1b
C4m1llyS4nt0s <145611862+C4m1llyS4nt0s@users.noreply.github.com>
2023-12-06 12:49:21 -0300

    index1.js

--- index1.js
+++ index1.js
@@ -3,17 +3,17 @@ const btnVoltar = document.querySelector('#prev-button');
 const btnIr = document.querySelector('#next-button');
 
 btnVoltar.addEventListener('click', function() {
-    if (slide.getAttribute('src') === "img1/feedback.png") {
-        slide.setAttribute('src', 'img1/feedback2..png');
-    } else if (slide.getAttribute('src') === "img1/feedback2..png") {
-        slide.setAttribute('src', 'img1/feedback.png');
+    if (slide.getAttribute('src') === "img/feedback.png") {
+        slide.setAttribute('src', 'img/feedback1..png');
+    } else if (slide.getAttribute('src') === "img/feedback1..png") {
+        slide.setAttribute('src', 'img/feedback.png');
     }
 });
 
 btnIr.addEventListener('click', function() {
-    if (slide.getAttribute('src') === "img1/feedback.png") {
-        slide.setAttribute('src', 'img1/feedback2..png');
-    } else if (slide.getAttribute('src') === "img1/feedback2..png") {
-        slide.setAttribute('src', 'img1/feedback.png');
+    if (slide.getAttribute('src') === "img/feedback.png") {
+        slide.setAttribute('src', 'img/feedback1..png');
+    } else if (slide.getAttribute('src') === "img/feedback1..png") {
+        slide.setAttribute('src', 'img/feedback.png');
     }
 });
